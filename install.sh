665a6309f5f4978ba2efff39cdbbfb64fc140f90
cieffelle <lorenzo.cicuttin@recrowd.com>
2024-03-04 09:11:53 +0100

    install script

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+fatal() {
+	echo "$@"
+	exit 1
+}
+
+dir=$(dirname $0)
+
+sed -i -e "s|HOME|$HOME|g" "$dir/lib/config.hpp" || fatal "Failed to set home directory"
+
+make || fatal "Failed to build junkdb"
+
+if [[ -d ~/.junkdb ]]; then
+	rm -rf ~/.junkdb || fatal "Failed to remove ~/.junkdb"
+fi
+
+mkdir ~/.junkdb || fatal "Failed to create ~/.junkdb"
+mkdir ~/.junkdb/data || fatal "Failed to create ~/.junkdb/data"
+
+cp "$dir/server/bin" ~/.junkdb/junkdb || fatal "Failed to copy junkdb server"
+cp "$dir/client/bin" ~/.junkdb/junkdb-client || fatal "Failed to copy junkdb client"
+
+if ! [[ "$(cat ~/.bashrc)" == *junkdb* ]]; then
+	echo -e 'export PATH="$PATH:$HOME/.junkdb"' >>~/.bashrc || fatal "Failed to add junkdb to PATH"
+fi
