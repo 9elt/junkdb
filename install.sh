e2366bbca5dfe5d00cf6f4e2cf0084429a6eccc8
9elt <nelt.primary@gmail.com>
2024-03-31 14:13:34 +0200

    install

--- install.sh
+++ install.sh
@@ -1,29 +1,29 @@
 #!/bin/sh
 
-fatal() {
+fail() {
 	echo "$@"
 	exit 1
 }
 
 dir=$(dirname $0)
 
-sed -i -e "s|HOME|$HOME|g" "$dir/lib/config.hpp" || fatal "Failed to set home directory"
+sed -i -e "s|HOME|$HOME|g" "$dir/lib/config.hpp" || fail "Failed to set home directory"
 
-make clean || fatal "Failed to clean junkdb"
-make || fatal "Failed to build junkdb"
+make clean || fail "Failed to clean junkdb"
+make || fail "Failed to build junkdb"
+
+sed -i -e "s|$HOME|HOME|g" "$dir/lib/config.hpp" || fail "Failed to reset config"
 
 if [[ -d ~/.junkdb ]]; then
-	rm -rf ~/.junkdb || fatal "Failed to remove ~/.junkdb"
+	rm -rf ~/.junkdb || fail "Failed to remove ~/.junkdb"
 fi
 
-mkdir ~/.junkdb || fatal "Failed to create ~/.junkdb"
-mkdir ~/.junkdb/data || fatal "Failed to create ~/.junkdb/data"
+mkdir ~/.junkdb || fail "Failed to create ~/.junkdb"
+mkdir ~/.junkdb/data || fail "Failed to create ~/.junkdb/data"
 
-cp "$dir/junkdb/bin" ~/.junkdb/junkdb || fatal "Failed to copy junkdb server"
-cp "$dir/junkdb-cli/bin" ~/.junkdb/junkdb-cli || fatal "Failed to copy junkdb client"
+cp "$dir/junkdb/bin" ~/.junkdb/junkdb || fail "Failed to copy junkdb server"
+cp "$dir/junkdb-cli/bin" ~/.junkdb/junkdb-cli || fail "Failed to copy junkdb client"
 
 if ! [[ "$(cat ~/.bashrc)" == *junkdb* ]]; then
-	echo -e 'export PATH="$PATH:$HOME/.junkdb"' >>~/.bashrc || fatal "Failed to add junkdb to PATH"
+	echo -e 'export PATH="$PATH:$HOME/.junkdb"' >>~/.bashrc || fail "Failed to add junkdb to PATH"
 fi
-
-sed -i -e "s|$HOME|HOME|g" "$dir/lib/config.hpp" || fatal "Failed to set home directory"
